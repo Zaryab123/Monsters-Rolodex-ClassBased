ed5cca9ba4c84f11d4f0efdf811c3f85eb542a83
Zaryab Raza <zaryab.baloch@expertflow.com>
2023-06-08 17:09:09 +0500

    React JS First Project with Class Based Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import {Component} from 'react';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(){
+
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount(){
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+      this.setState(() => {
+        return {monsters: users}
+      })
+    })
+  }
+
+  onSearchChange = (event) => {
+
+    let searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
+  render (){
+
+    let { monsters, searchField } = this.state;
+    let { onSearchChange } = this;
+
+    let filteredList = monsters.filter(monster => {
+         return monster.name.toLowerCase().includes(searchField);
+    })
+
+    return(
+      <div className='App'>
+
+          <h1 className='app-title'>Monsters Rolodex</h1>
+          <SearchBox onChangeHandler={onSearchChange} placeholder={'Search Monsters'} className={'monster-search-box'}/>
+         
+          {
+            <CardList monsters={filteredList}/>
+          }
+      </div>
+    )
+  }
 }
+
 
 export default App;
